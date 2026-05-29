83863f7e34f73cd293f7e788759d6bd348912855
cristopher Mendoza <cristopher.mendoza007@gmail.com>
2026-05-29 16:02:23 -0500

    primero

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Bienvenidos al taller de React</h1>
+      <p>Mi nombre es Cristopher Xavier Mendoza Naula</p>
+    </>
+  )
+}
+
+export default App
